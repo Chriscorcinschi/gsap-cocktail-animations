3a7d309753f909eef342fa1a7ed4b836fe9b0aec
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2025-08-07 16:19:57 +0200

    implement the DrinksMenu section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
+import DrinksMenu from "./components/DrinksMenu.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
       <Cocktails/>
       <About />
       <Art />
+      <DrinksMenu />
     </main>
   )
 }
