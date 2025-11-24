17f2e3897fabb1cc7b85ee839dd2aeddc3c20f60
Chriscorcinshci <chriscorcinschi.20@gmail.com>
2025-11-24 10:32:45 +0100

    fix base!

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://chriscorcinschi.github.io/gsap-cocktail-animations/",
+  base: "/gsap-cocktail-animations/",
   plugins: [
     react(), 
     tailwindcss(),
